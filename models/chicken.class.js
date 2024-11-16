bf8306cd262fd543ebb95347d03f6f30201f866b
DerBlockProgrammer <marcvossler@outlook.de>
2024-11-16 21:16:15 +0100

    newFunction

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -7,6 +7,7 @@ class Chicken extends MovableObject{
         'img/3_enemies_chicken/chicken_normal/1_walk/2_w.png',
         'img/3_enemies_chicken/chicken_normal/1_walk/3_w.png'
     ];
+    walking_chicken = new Audio('audio/chicken.mp3')
   
     constructor(){
         super().loadImage('img/3_enemies_chicken/chicken_normal/1_walk/1_w.png');
@@ -15,6 +16,12 @@ class Chicken extends MovableObject{
         this.x = 200 + Math.random() * 500;
         this.speed = 0.15 + Math.random() * 0.5;
         this.animate();
+        
+        
+        this.walking_chicken.volume=0.10;
+        this.walking_chicken.play();
+        this.walking_chicken.pause();
+        
        }
 
        animate() {
@@ -26,6 +33,6 @@ class Chicken extends MovableObject{
             this.currentImage++;
         }, 1000);
     }
-
+    
      
 }
