fdfbf32f44f860617eb626342b22d03d1c362eb9
DerBlockProgrammer <marcvossler@outlook.de>
2024-11-19 11:05:49 +0100

    colliing

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -44,6 +44,15 @@ class MovableObject {
     }
 
     }
+
+
+    isColling(mo){
+        return this.x + this.width > mo.x &&
+        this.y + this.height > mo.y &&
+        this.x < mo.x &&
+        this.y < mo.y + mo.height
+    }
+    
     loadImages(arr) {
         arr.forEach((path) => {
             let img = new Image();
